891d9baaed33837aa9ca8963aa0907535843e18f
jasper126 <159818033+jasper126@users.noreply.github.com>
2024-02-13 15:53:10 +0100

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,26 +1,12 @@
 #VPC called (my_vpc) 
 resource "aws_vpc" "my_vpc" {
-  cidr_block             = "172.16.0.0/16"
+  cidr_block             = var.vpc_cidr_block
   enable_dns_support     = true
   enable_dns_hostnames   = true
   tags = {
     Name = "test_vpc"
   }
 }
-#Elastic IP for NAT
-resource "aws_eip" "nat_eip" {
-  domain        = "vpc"
-}
-# NAT 
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public-subnet.id
-  depends_on    = [aws_internet_gateway.internet-gateway]
-  tags = {
-    Name        = "nat"
-  }
-}
-
 #Internet GW
 resource "aws_internet_gateway" "internet-gateway" {
   vpc_id = aws_vpc.my_vpc.id
@@ -29,37 +15,40 @@ resource "aws_internet_gateway" "internet-gateway" {
 #Public subnet
 resource "aws_subnet" "public-subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
-  availability_zone       = "us-east-1b"
-  cidr_block              = "172.16.11.0/24"
+  availability_zone       = var.availability_zone_public
+  cidr_block              = var.public_subnet_cidr_block
   map_public_ip_on_launch = true
   tags = {
     Name = "public subnet"
   }
 }
+#private Subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = var.private_subnet_cidr_block
+  availability_zone       = var.availability_zone_private
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "Private subnet"
+  }
+
+}
 # Route table public
 resource "aws_route_table" "public-route-table" {
   vpc_id = aws_vpc.my_vpc.id
 }
-
+# Private route table
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.my_vpc.id
+}
 # Route for public subnet
 resource "aws_route" "public-subnet-route" {
   route_table_id         = aws_route_table.public-route-table.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.internet-gateway.id
 }
-# Private route table
-resource "aws_route_table" "private-route-table" {
-  vpc_id = aws_vpc.my_vpc.id
-}
 
-# Route for private subnet (using NAT Gateway)
-resource "aws_route" "private-subnet-route" {
-  route_table_id         = aws_route_table.private-route-table.id
-  destination_cidr_block = "172.16.10.0/24"
-  nat_gateway_id         = aws_nat_gateway.nat.id
-}
-
-#route table assiocioation
+# Route table assiocioation ########################################
 #public
 resource "aws_route_table_association" "public-subnet-route-table-assiocioation" {
   subnet_id      = aws_subnet.public-subnet.id
@@ -70,15 +59,7 @@ resource "aws_route_table_association" "private-subnet-route-table-assiocioation
   subnet_id      = aws_subnet.my_subnet.id
   route_table_id = aws_route_table.private-route-table.id
 }
-
-#private Subnet
-resource "aws_subnet" "my_subnet" {
-  vpc_id                  = aws_vpc.my_vpc.id
-  cidr_block              = "172.16.10.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = false
-}
-
+#END Route table assiocioation ########################################
 
 #private interface1 
 resource "aws_network_interface" "ec2_int1" {
@@ -99,12 +80,35 @@ resource "aws_security_group" "first" {
   name   = "jasper SG"
 
 ingress {
-from_port   = 0
-to_port     = 0
-protocol    = "-1"
+from_port   = 80
+to_port     = 80
+protocol    = "TCP"
 cidr_blocks = ["0.0.0.0/0"]
 }
-
+ingress {
+from_port   = 443
+to_port     = 443
+protocol    = "TCP"
+cidr_blocks = ["0.0.0.0/0"]
+}
+ingress {
+from_port   = 3306
+to_port     = 3306
+protocol    = "TCP"
+cidr_blocks = ["0.0.0.0/0"]
+}
+ingress {
+from_port   = 22
+to_port     = 22
+protocol    = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+ingress {
+from_port   = 3389
+to_port     = 3389
+protocol    = "TCP"
+cidr_blocks = ["0.0.0.0/0"]
+}
 #all outbound
 egress {
 from_port   = 0
@@ -114,4 +118,3 @@ cidr_blocks = ["0.0.0.0/0"]
 
   }
 }
-
