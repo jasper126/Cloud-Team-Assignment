aad520e32da18ae6b86ef272b5cb62ef801d1b48
jasper126 <159818033+jasper126@users.noreply.github.com>
2024-02-13 15:54:23 +0100

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -13,6 +13,21 @@ pip install mysql-connector-python
 sudo yum -y install httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
+cd /var/www/html/
+# Create a simple HTML file
+echo '<!DOCTYPE html>' > index.html
+echo '<html lang="en">' >> index.html
+echo '<head>' >> index.html
+echo '    <meta charset="UTF-8">' >> index.html
+echo '    <meta name="viewport" content="width=device-width, initial-scale=1.0">' >> index.html
+echo '    <title>Hello, CloudTeam!</title>' >> index.html
+echo '</head>' >> index.html
+echo '<body>' >> index.html
+echo '    <h1>Hello, CloudTeam!</h1>' >> index.html
+echo '    <p>This is a simple HTML page served by Apache HTTP Server on Amazon Linux.</p>' >> index.html
+echo '</body>' >> index.html
+echo '</html>' >> index.html
+sudo service httpd restart
 
 #MySQL installation 
 sudo dnf -y install mariadb105
